c12d3f54d27b7adb08af2b493c156f0f1cc7792f
snowfag <yuki@lolicon.eu>
2019-11-18 00:22:00 -0700

    v2.1

--- amazon_url_cleaner.user.js
+++ amazon_url_cleaner.user.js
@@ -4,9 +4,8 @@
 // @namespace https://github.com/snowfag/userscripts
 // @author snowfag
 // @description Cleans all the crud out of an Amazon item URL to make it as short as possible
-// @match *://*.amazon.com/*/dp/*
-// @match *://*.amazon.com/gp/product/*
-// @version        2.0
+// @include /^https?:\/\/(www|smile)\.amazon\.com\/(.*\/)?(dp|gp\/product)\/[A-Z0-9]{10}(\/.*)?$/
+// @version        2.1
 // ==/UserScript==
 var newurl = 'https://smile.amazon.com/dp/' + document.getElementById('ASIN').value;
 if (newurl != document.URL) location.replace(newurl);
