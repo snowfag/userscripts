2d79b9d9a4da7df99a8d1002b479c1b3ef00a5bd
snowfag <yuki@lolicon.eu>
2019-11-20 17:23:09 -0700

    add update url for :reasons:

--- amazon_url_cleaner.user.js
+++ amazon_url_cleaner.user.js
@@ -5,7 +5,8 @@
 // @author snowfag
 // @description Cleans all the crud out of an Amazon item URL to make it as short as possible
 // @include /^https?:\/\/(www|smile)\.amazon\.com\/(.*\/)?(dp|gp\/product)\/[A-Z0-9]{10}(\/.*)?$/
-// @version        2.1
+// @version 2.2
+// @updateURL https://github.com/snowfag/userscripts/raw/master/amazon_url_cleaner.user.js
 // ==/UserScript==
 var newurl = 'https://smile.amazon.com/dp/' + document.getElementById('ASIN').value;
 if (newurl != document.URL) location.replace(newurl);
