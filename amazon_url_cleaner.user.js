b48dad37293f4cf2ff2502d7b8e4b5dc91051efc
Yukikaze <yuki@lolicon.eu>
2020-03-03 22:01:08 -0700

    version bump
    would help if I actually remembered to do that

--- amazon_url_cleaner.user.js
+++ amazon_url_cleaner.user.js
@@ -5,7 +5,7 @@
 // @author snowfag
 // @description Cleans all the crud out of an Amazon item URL to make it as short as possible
 // @include /^https?:\/\/(www|smile)\.amazon\.com\/(.*\/)?(dp|gp\/product)\/[A-Z0-9]{10}(.*)?$/
-// @version 2.3
+// @version 2.4
 // @updateURL https://github.com/snowfag/userscripts/raw/master/amazon_url_cleaner.user.js
 // ==/UserScript==
 history.replaceState(null, "", "/dp/" + document.getElementById('ASIN').value);
