ef26545bb20059d9be3c157fe09d40c483fe3cac
Yukikaze <yuki@lolicon.eu>
2020-03-03 22:05:23 -0700

    Update amazon_url_cleaner.user.js

--- amazon_url_cleaner.user.js
+++ amazon_url_cleaner.user.js
@@ -5,7 +5,7 @@
 // @author snowfag
 // @description Cleans all the crud out of an Amazon item URL to make it as short as possible
 // @include /^https?:\/\/(www|smile)\.amazon\.com\/(.*\/)?(dp|gp\/product)\/[A-Z0-9]{10}(.*)?$/
-// @version 2.4
+// @version 2.5
 // @updateURL https://github.com/snowfag/userscripts/raw/master/amazon_url_cleaner.user.js
 // ==/UserScript==
-history.replaceState(null, "", "/dp/" + document.getElementById('ASIN').value);
+history.replaceState(null, "Amazon URL Cleaner", "/dp/" + document.getElementById('ASIN').value);
