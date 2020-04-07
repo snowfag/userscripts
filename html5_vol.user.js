44eae316cf85f8955d6e860153a810d60a9fa624
snowfag <yuki@lolicon.eu>
2020-04-06 22:00:07 -0700

    oops

--- html5_vol.user.js
+++ html5_vol.user.js
@@ -3,7 +3,7 @@
 // @name HTML5 av default volume changer
 // @namespace https://github.com/snowfag/userscripts
 // @author snowfag
-// @description Cleans all the crud out of an Amazon item URL to make it as short as possible
+// @description Fixes html5 videos to not be loud as hell.
 // @include /^https?:\/\/i.lolicon.eu\/.*$/
 // @include /^https?:\/\/www.tiktok.com/.*$/
 // @version 1.0
