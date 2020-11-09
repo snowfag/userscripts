67c6a0b68a9a0b6578855b7d1bedb6f8d0cd46ac
snowfag <yuki@lolicon.eu>
2020-11-09 15:34:47 -0700

    fix for longer item code lengths.

--- aliexpress_url_cleaner.user.js
+++ aliexpress_url_cleaner.user.js
@@ -4,8 +4,8 @@
 // @namespace https://github.com/snowfag/userscripts
 // @author snowfag
 // @description Cleans all the crud out of an aliexpress item URL to make it as short as possible
-// @include /^https?:\/\/www\.aliexpress\.com\/item\/[0-9]{11,13}\.html\?.*$/
-// @version 1.2
+// @include /^https?:\/\/www\.aliexpress\.com\/item\/[0-9]{11,}\.html\?.*$/
+// @version 1.4
 // @updateURL https://github.com/snowfag/userscripts/raw/master/aliexpress_url_cleaner.user.js
 // ==/UserScript==
 history.replaceState(null, "Aliexpress URL Cleaner", window.location.pathname);
