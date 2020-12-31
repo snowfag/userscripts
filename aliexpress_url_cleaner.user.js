3049dd6116eb4585d5785c0eb4011aee8650799d
snowfag <yuki@lolicon.eu>
2020-12-30 22:20:01 -0700

    cocks

--- aliexpress_url_cleaner.user.js
+++ aliexpress_url_cleaner.user.js
@@ -4,7 +4,7 @@
 // @namespace https://github.com/snowfag/userscripts
 // @author snowfag
 // @description Cleans all the crud out of an aliexpress item URL to make it as short as possible
-// @include /^https?:\/\/www\.aliexpress\.com\/item\/[0-9]{11,}\.html\?.*$/
+// @include /^https?:\/\/www\.aliexpress\.com\/item\/[0-9]{8,}\.html\?.*$/
 // @version 1.4
 // @updateURL https://github.com/snowfag/userscripts/raw/master/aliexpress_url_cleaner.user.js
 // ==/UserScript==
